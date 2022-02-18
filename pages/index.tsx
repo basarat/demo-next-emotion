46f07333ae56a710ae93c50c17de24012a38bb09
basarat <basaratali@gmail.com>
2022-02-18 16:43:14 +1100

    🌹

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  cursor: pointer;
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`;
 
 const Home: NextPage = () => {
   return (
-    null
+    <Button>Styled by emotion</Button>
   );
 }
 
